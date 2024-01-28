54c0decbfa9a0af3f44a37f1f68569c0624cb7bd
Mafer-Rodriguez <utm22030609@utma.edu.mx>
2024-01-27 22:36:04 -0600

    feat: new fuction to remove user by index

--- functions_api.js
+++ functions_api.js
@@ -1,4 +1,4 @@
-//takes one username and return it if exists.
+ //takes one username and return it if exists.
 const username=["Maricruz","Lorena","Camila","Brandon"]
 function getUser(getUser){
   if(username.includes(getUser)){
@@ -22,3 +22,22 @@ function addUser (newUser){
   return username;
 }
 console.log(addUser("Avril"));
+
+
+//takes an index and, if found, removes the element from the array, it returns the deleted element and the new array.
+
+const myIndex=["Maricruz","Lorena","Camila","Brandon"];
+
+function removeUserByIndex(index){
+  if(index >= 0 && index < myIndex.length){//if the index is greater than 0 and less than the length of the array
+    const indexRemoved = myIndex.splice(index, 1);//splice method removes the element from the array
+    return indexRemoved;
+  } else {
+    return "User not found";
+  }
+}
+
+console.log(removeUserByIndex(2));
+console.log(myIndex);
+
+
