ee044d0c05d45f431ffcd1a48768d383429deec6
Mafer-Rodriguez <utm22030609@utma.edu.mx>
2024-01-28 23:17:33 -0600

    feat: latest arrow functions added

--- arrow_functions_api.js
+++ arrow_functions_api.js
@@ -52,7 +52,7 @@ console.log(getUser("Mont"));
 
 
 //getUsers, return all existing users
-const getAllUsers = () => {// remenber => is the arrow function, it is a shorter way to write a function
+const getAllUsers = (getAllUser) => {// remenber => is the arrow function, it is a shorter way to write a function
   try {
     if (users.length > 0) {//length method returns the number of elements in the array  
       return sendReponse(200, users);
@@ -63,7 +63,7 @@ const getAllUsers = () => {// remenber => is the arrow function, it is a shorter
     return sendReponse(500, error);
   }
 };
-console.log(getAllUsers());
+console.log(getAllUsers(getAllUsers));
 
 //addUser, adds a new user to the users array and return the user created, all users in new array and the user created
 const addUser = (newUser) => {
@@ -95,3 +95,54 @@ const removeUserByIndex = (index) => {
 console.log(removeUserByIndex(2));
 console.log("New array: "+ users);
 
+// removeLastUser, removes the last element from the array, it returns the deleted element and the new array.
+const removeLastUser = (removeLastUser) => {
+  try {
+    const user = users.pop();//pop method removes the last element from an array and returns that element
+    return sendReponse(200, user);
+  } catch (error) {
+    return sendReponse(500, error);
+  }
+};
+console.log(removeLastUser(removeLastUser));
+console.log("New array: "+ users);
+
+//removeFirstUser, removes the first element from the array, it returns the deleted element and the new array.
+const removeFirstUser = (removeFirstUser) => {
+  try {
+    const user = users.shift();//shift method removes the first element from an array and returns that removed element
+    return sendReponse(200, user);
+  } catch (error) {
+    return sendReponse(500, error);
+  }
+}
+console.log(removeFirstUser(removeFirstUser));
+console.log("New array: "+ users);
+
+
+//updateUserByIndex, takes the index and the new value, if index exists then replace the element with the new value.
+const updateUserByIndex = (index, newValue) => {
+  try {
+    if (index >= 0 && index < users.length) {
+      const user = users.splice(index, 1, newValue);
+      return sendReponse(200, user);
+    }
+
+    return sendReponse(404);//if the index does not exist (I don't know if it is the correct way to write it or if it goes in this part)
+  } catch (error) {
+    return sendReponse(500, error);
+  }
+};
+console.log(updateUserByIndex(1, "Sofia"));
+console.log("New array: "+ users);
+
+
+//getUsersSize, return the number of users in the array.
+const getUsersSize = (getUsersSize) => {
+  try {
+    return sendReponse(200, users.length);
+  } catch (error) {
+    return sendReponse(500, error);
+  }
+};
+console.log(getUsersSize(getUsersSize));
